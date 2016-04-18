1682a8b44cc945de55fe8d2466849b88e7c6a5be
Aditi Nair <asn264@nyu.edu>
2016-04-18 13:27:38 -0400

    finished stopwords analysis

--- custom_stopwords/generate_stop_words.py
+++ custom_stopwords/generate_stop_words.py
@@ -1,30 +1,58 @@
 '''
 Sample new dataset from sqlite database in proportion to subreddit prevalence in database. 
-Make sure to drop empty comments as well as comments that are not in English characters. 
+Make sure to drop empty comments and deleted comments as well as comments that are not in English characters. 
 Do word counts using Collections class.
 '''
 
+from collections import Counter
 import pandas as pd
 import sqlite3
-
-total_sample_size = 10**6
-
-subreddit_data = pd.read_csv('../exploratory_analysis/subreddit_count.tsv',delimiter='\t')
-top_250 = subreddit_data[:250][[1,3]]
-sum_percents = top_250['percents'].sum(index=1)
-top_250['percents'] = top_250['percents']/sum_percents*total_sample_size
+import pickle
+import sys
 
 
-with sqlite3.connect('../exploratory_analysis/database.sqlite') as conn:
-	with open('comments_data','a') as out_f:
+def generate_counts():
+
+	'''
+	Sample from dataset in proportion to subreddit counts. Then take word counts.
+	'''
+
+	total_sample_size = 10**6
+
+	subreddit_data = pd.read_csv('../exploratory_analysis/subreddit_count.tsv',delimiter='\t')
+	top_250 = subreddit_data[:250][[1,3]]
+	sum_percents = top_250['percents'].sum(index=1)
+	top_250['percents'] = top_250['percents']/sum_percents*total_sample_size
+
+
+	#Start with empty list
+	global_li = []
+	with sqlite3.connect('../exploratory_analysis/database.sqlite') as conn:
 
 		print 'Opened connection'
-
 		cursor = conn.cursor()
 
 		for i in range(len(top_250)):
+
+			print 'Querying subreddit ', i
+
 			table = pd.read_sql_query("SELECT subreddit,body FROM May2015 WHERE length(body)>0 AND body <> '[deleted]' AND subreddit='"+top_250.ix[i,0] +"' LIMIT "+str(int(top_250.ix[i,1])), conn)
-			
-			table.to_csv(out_f, sep='\t',header=False,encoding='utf-8',index=False)
+
+			#Loop through rows of table and extract comment
+			for i in range(len(table)):
+
+				#Append words to list
+				global_li.extend(table['body'][i].strip().split())
+
+	pickle.dump(Counter(global_li), open("counts_"+str(total_sample_size)+".p", "wb"))
+
+def get_top_n():
+
+	'''
+	Open pickle file and get top N words by count.
+	'''
+
+	counts = pickle.load(open('counts_1000000.p', 'rb'))
+	counts.most_common(100)
 
 
